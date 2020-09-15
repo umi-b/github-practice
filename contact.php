26465091cdd5689e8af2c556b7f8fdf9aed8c5de
u_mi <miohys0528@gmail.com>
2020-09-15 15:34:00 +0900

    'コンタクトフォーム'

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>コンタクトフォーム</title>
+</head>
+<body>
+  <form action="">
+    <div class="container">
+      <div class="low">
+        <div class="box1">box1</div>
+        <div class="box2">box2</div>
+        <div class="box3">box3</div>
+      </div>
+    </div>
+  </form>
+  
+</body>
+</html>
